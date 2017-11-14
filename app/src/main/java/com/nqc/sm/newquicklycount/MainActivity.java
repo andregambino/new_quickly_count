99d6de9f8f8294bbd6e3b518d7a46301fe2b43df
Andrea Gambino <andreagambino@MacBook-Pro-di-Andrea.local>
2017-11-14 13:07:51 +0100

    Initial commit

---
+++ app/src/main/java/com/nqc/sm/newquicklycount/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nqc.sm.newquicklycount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CIAO alla seconda
+        sarà quel che sarà ???
+
+    }
+}
